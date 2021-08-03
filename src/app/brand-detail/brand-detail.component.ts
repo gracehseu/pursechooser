1dfa907608d899ac9d9d07a497fd7a8b9dfd0c63
Grace Hseu <25470155+gracehseu@users.noreply.github.com>
2021-08-03 01:17:13 -0500

    added price and phone // add dropdown for advanced

--- src/app/brand-detail/brand-detail.component.ts
+++ src/app/brand-detail/brand-detail.component.ts
@@ -25,7 +25,13 @@ export class BrandDetailComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      brandListArray: this.fb.array([])
+      brandListArray: this.fb.array([]),
+      phone: new FormControl(null),
+      l: new FormControl(null),
+      w: new FormControl(null),
+      h: new FormControl(null),
+      maxPrice: new FormControl(null),
+      minPrice: new FormControl(null),
     })
   }
 
@@ -36,7 +42,7 @@ export class BrandDetailComponent implements OnInit {
 
   onSubmit() {
     console.log("submit")
-    console.log(this.form);
+    console.log(this.form.value);
   }
 
   onClear() {
@@ -47,6 +53,47 @@ export class BrandDetailComponent implements OnInit {
   private initForm() {
     
   }
+
+  // (function () {
+  //   const parent = document.querySelector('.range-slider');
+
+  //   if (!parent) {
+  //     return;
+  //   }
+
+  //   const rangeS = parent.querySelectorAll('input[type="range"]'),
+  //     numberS = parent.querySelectorAll('input[type="number"]');
+
+  //   rangeS.forEach((el) => {
+  //     el.oninput = () => {
+  //       let slide1 = parseFloat(rangeS[0].value),
+  //         slide2 = parseFloat(rangeS[1].value);
+
+  //       if (slide1 > slide2) {
+  //         [slide1, slide2] = [slide2, slide1];
+  //       }
+
+  //       numberS[0].value = slide1;
+  //       numberS[1].value = slide2;
+  //     }
+  //   });
+
+  //   numberS.forEach((el) => {
+  //     el.oninput = () => {
+  //       let number1 = parseFloat(numberS[0].value),
+  //         number2 = parseFloat(numberS[1].value);
+
+  //       if (number1 > number2) {
+  //         let tmp = number1;
+  //         numberS[0].value = number2;
+  //         numberS[1].value = tmp;
+  //       }
+
+  //       rangeS[0].value = number1;
+  //       rangeS[1].value = number2;
+  //     }
+  //   });
+  // })();
 
   onCheckboxChange(e) {
     const brandListArray: FormArray = this.form.get('brandListArray') as FormArray;
